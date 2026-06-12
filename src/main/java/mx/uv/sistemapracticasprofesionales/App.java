d1573a853c7d9ecfc852583d6e4e47ef27fd0b6b
gaels-dev <gaelsamei@gmail.com>
2026-06-12 16:39:35 -0600

    Crear estructura base del proyecto

---
+++ src/main/java/mx/uv/sistemapracticasprofesionales/App.java
@@ -0,0 +1,39 @@
+package mx.uv.sistemapracticasprofesionales;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/FXMLInicioSesion.fxml"));
+
+            Scene escena = new Scene(root);
+            primaryStage.setScene(escena);
+            primaryStage.setTitle("Iniciar sesión - Sistema Practicas Profesionales");
+            primaryStage.setResizable(false);
+            primaryStage.sizeToScene();
+            primaryStage.show();
+            primaryStage.centerOnScreen();
+        } catch (IOException ex) {
+            ex.getMessage();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ src/main/java/mx/uv/sistemapracticasprofesionales/controller/FXMLInicioSesionController.java
@@ -0,0 +1,22 @@
+package mx.uv.sistemapracticasprofesionales.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author gaels
+ */
+public class FXMLInicioSesionController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
